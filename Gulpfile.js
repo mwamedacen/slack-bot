cf4535746841e441a2b6d41a29e6eb3cc6bb8716
Mohamed <mohamed@captaindash.com>
2018-01-19 14:06:33 +0100

    add gulp task for packaging zip for Microsoft Teams

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,21 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 
+const zip = require('gulp-zip');
+const del = require('del');
+
+gulp.task('clean', () => {
+  return del([
+    'manifest/**/*'
+  ])
+});
+
+gulp.task('generate-manifest', () => {
+  gulp.src(['platforms/MicrosoftTeams/images/contoso*', 'platforms/MicrosoftTeams/manifest.json'])
+  .pipe(zip('helloworldapp.zip'))
+  .pipe(gulp.dest('manifest'));
+});
+
 gulp.task('default', () => {
   gulp.src('app/properties/message')
   .pipe(gulp.dest('dist/app/properties'));
